20f53c34617cfb9f16bde00cfe2c87c8b4cf1cb5
unclejames2610 <udealaebuka@gmail.com>
2024-08-30 14:09:19 +0100

    feat:mutation added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 function App() {
   const todoData = useQuery({
     queryKey: ["todo"],
     queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
         res.json()
       ),
+  });
+
+  const { mutate, isPending, isError, isSuccess } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      }).then((res) => res.json()),
   });
 
   // console.log(data);
 
   if (todoData.isLoading) return <div>Loading...</div>;
 
-  if (todoData.error) return <div>error fetching data</div>;
+  if (todoData.error || isError) return <div>an error occurred</div>;
 
   return (
     <div className="App">
+      {isPending && <p>Data is being added...</p>}
+      <button
+        onClick={() =>
+          mutate({
+            userId: 14,
+            id: 800,
+            title: "Edit",
+            body: "new stuff for body",
+          })
+        }
+      >
+        Add Post
+      </button>
       {todoData.data.map((todo, index) => (
         <div key={index}>
-          <h1>id: {todo?.id}</h1>
+          <h4>id: {todo?.id}</h4>
           <h1>id: {todo?.title}</h1>
+          <p> {todo.body}</p>
         </div>
       ))}
     </div>
